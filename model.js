9783fa9a961eb6f27779503205a8fa54495c6c12
Mathieu Sylvain <mathieu@ti-coco.com>
2013-02-19 13:51:31 -0500

    First Commit

---
+++ model.js
@@ -0,0 +1,14 @@
+model = {
+	booleans: {
+		isTrue: true,
+		isFalse: false
+	},
+	page: {
+		title: "Évitez les surprises... Payez le même montant à chaque mois."
+	},
+	items: [
+       	"Contrôler vos dépenses, étalez votre paiement de facture.",
+       	"Sachez à l'avance le montant de votre facture.",
+       	"Recevez une facture par mois plutôt qu'aux deux mois."
+	]
+};
---
+++ model2.js
@@ -0,0 +1,14 @@
+model = {
+	booleans: {
+		isTrue: true,
+		isFalse: false
+	},
+	page: {
+		title: "PAGE 2."
+	},
+	items: [
+       	"Lorem ipsum dolor sit amet...",
+       	"Contrôler vos dépenses, étalez votre paiement de facture.",
+       	"Recevez une facture par mois plutôt qu'aux deux mois."
+	]
+};
